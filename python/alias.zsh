45d562f7de806617ca463ef4841fda74b850174d
wensontsai <wensontsai@gmail.com>
2020-01-22 22:51:28 -0500

    adding aliases for python3 pip3, adding scikit-learn

---
+++ python/alias.zsh
@@ -0,0 +1,3 @@
+# alias python/ pip to point to python3/ pip3
+alias python='/usr/local/bin/python3'
+alias pip='/usr/local/bin/pip3'
--- python/install.sh
+++ python/install.sh
@@ -1,2 +1,5 @@
-#### INSTALL PYTHON 3.7.4 with ANACONDA
-# brew cask install anaconda
+#### ANACONDA
+brew cask install anaconda
+
+### install python 3.7.4
+pip3 install -U numpy scipy scikit-learn

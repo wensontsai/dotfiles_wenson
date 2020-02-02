13e3b91b4792a86dc3f74b16081194f2efd1cd41
wensontsai <wensontsai@gmail.com>
2020-02-02 15:19:00 -0500

    vscode package installs

---
+++ vscode/install.sh
@@ -0,0 +1,10 @@
+# after vscode is installed with `code` command working...
+
+# run these to install necessary packages
+# code --install-extension apollographql.vscode-apollo
+# code --install-extension dsznajder.es7-react-js-snippets
+# code --install-extension justusadam.language-haskell
+# code --install-extension kumar-harsh.graphql-for-vscode
+# code --install-extension ms-azuretools.vscode-docker
+# code --install-extension ms-python.python
+# code --install-extension naumovs.theme-oceanicnext

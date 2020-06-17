359c46fa89e3db76b75dcf0e75791b68dcf7340e
wensontsai <wensontsai@gmail.com>
2020-06-17 13:19:57 -0400

    vscode extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,14 +1,31 @@
 # after vscode is installed with `code` command working...
 
 # run these to install necessary packages
+code --install-extension naumovs.theme-oceanicnext
 code --install-extension apollographql.vscode-apollo
+code --install-extension atlassian.atlascode
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension eg2.tslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.vscode-pull-request-github
+code --install-extension heptio.jsonnet
+code --install-extension HookyQR.beautify
 code --install-extension justusadam.language-haskell
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
+code --install-extension ms-vscode.sublime-keybindings
 code --install-extension naumovs.theme-oceanicnext
-
+code --install-extension redhat.java
+code --install-extension redhat.vscode-yaml
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
 # find `settings.json` here:
 cp -rp ./settings.json /Users/$USER/Library/ApplicationSupport/Code/User/settings.json
 
